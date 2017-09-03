115d72569cc73251f04ab987590c4edbfe6bbebd
making <makingx@gmail.com>
2017-09-03 23:46:06 +0900

    Remove default-filters and add CorsFilter

--- src/main/java/jsug/portside/portsidegateway/PortsideGatewayApplication.java
+++ src/main/java/jsug/portside/portsidegateway/PortsideGatewayApplication.java
@@ -3,6 +3,9 @@ package jsug.portside.portsidegateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.server.WebFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,18 @@ public class PortsideGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortsideGatewayApplication.class, args);
+	}
+
+	@Bean
+	WebFilter corsFilter() {
+		return (exchange, chain) -> {
+			HttpHeaders headers = exchange.getResponse().getHeaders();
+			headers.add("Access-Control-Allow-Origin", "*");
+			headers.add("Access-Control-Allow-Methods",
+					"OPTIONS, POST, GET, OPTIONS, PUT, DELETE");
+			headers.add("Access-Control-Max-Age", "3600");
+			headers.add("Access-Control-Allow-Headers", "*");
+			return chain.filter(exchange);
+		};
 	}
 }
